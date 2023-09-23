62ef7c663bd52c59cc6927fa4387875af2f2eca6
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-09-23 16:11:31 +0300

    01gallery update, .json update, simplelightbox download, etc...

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+const imageHtml = galleryItems
+    .map(
+        ({ original, preview, description }) =>
+     `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" 
+           alt="${description}" />
+   </a>
+</li>`
+  ).join('');
+
+galleryEl.insertAdjacentHTML("beforeend", imageHtml);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+
+  closeText: "×",
+  nav: true,
+  animationSlide: true,
+  animationSpeed: 250,
+});
