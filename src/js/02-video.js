6c495b1526b6f0e17673841b2702c03b42b4deed
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-09-24 14:21:31 +0300

    fixation of 02.js and new 03.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash/throttle';
 
+const player = new Player('vimeo-player', {
+  id: '236203659', 
+  width: 640,
+  height: 360,
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime !== null) {
+  player.setCurrentTime(parseFloat(savedTime));
+}
+
+player.on('timeupdate', throttle(function (event) {
+  const currentTime = event.seconds;
+  localStorage.setItem('videoplayer-current-time', currentTime);
+}, 1000)); 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,55 @@
+// Import the throttle function from the lodash library
+import throttle from 'lodash/throttle';
 
+// Check if the browser supports local storage
+if (typeof Storage !== 'undefined') {
+  // Select the feedback form and its input fields
+  const form = document.querySelector('.feedback-form');
+  const emailInput = form.querySelector('input[name="email"]');
+  const messageTextarea = form.querySelector('textarea[name="message"]');
+
+  // Retrieve the saved state from local storage or initialize an empty object
+  const savedState =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || {};
+
+  // Populate the email and message fields with saved values if available
+  emailInput.value = savedState.email || '';
+  messageTextarea.value = savedState.message || '';
+
+  // Create a throttled function to save form state to local storage with a 500ms delay
+  const saveStateWithThrottle = throttle(() => {
+    // Create an object with the current email and message values
+    const state = {
+      email: emailInput.value,
+      message: messageTextarea.value,
+    };
+    // Store the state object as a JSON string in local storage
+    localStorage.setItem('feedback-form-state', JSON.stringify(state));
+  }, 500);
+
+  // Add input event listeners to email and message fields to trigger state saving
+  emailInput.addEventListener('input', saveStateWithThrottle);
+  messageTextarea.addEventListener('input', saveStateWithThrottle);
+
+  // Add a submit event listener to the form
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+
+    // Remove the saved state from local storage
+    localStorage.removeItem('feedback-form-state');
+
+    // Get the current values of the email and message fields
+    const email = emailInput.value;
+    const message = messageTextarea.value;
+
+    // Log the email and message values to the console
+    console.log({ email, message });
+
+    // Clear the email and message fields after submitting the form
+    emailInput.value = '';
+    messageTextarea.value = '';
+  });
+} else {
+  // If local storage is not supported, display an error message in the console
+  console.error('Local storage is not supported in this browser.');
+}
